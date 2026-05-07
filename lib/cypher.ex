a8d7ae9855e07d05f3b0914234709bb27afb7967
Matt Beanland <beanland@live.com.au>
2026-05-07 22:01:29 +0930

    extended cypher struct family and refactored Neo4jHelper to use it

--- lib/cypher.ex
+++ lib/cypher.ex
@@ -12,7 +12,10 @@ defmodule AshNeo4j.Cypher do
 
   require Logger
 
-  alias AshNeo4j.Cypher.{Query, Match, OptionalMatch, Where, With, Return, OrderBy, Skip, Limit}
+  alias AshNeo4j.Cypher.{
+    Query, Match, OptionalMatch, Create, Merge, Where, With,
+    Set, Remove, Delete, DetachDelete, Return, OrderBy, Skip, Limit
+  }
 
   @spec remove_properties(atom(), maybe_improper_list()) :: binary()
   @doc """
@@ -232,8 +235,14 @@ defmodule AshNeo4j.Cypher do
 
   defp render_clause(%Match{pattern: p}), do: "MATCH #{p}"
   defp render_clause(%OptionalMatch{pattern: p}), do: "OPTIONAL MATCH #{p}"
+  defp render_clause(%Create{pattern: p}), do: "CREATE #{p}"
+  defp render_clause(%Merge{pattern: p}), do: "MERGE #{p}"
   defp render_clause(%Where{conditions: conds}), do: "WHERE #{Enum.join(conds, " AND ")}"
   defp render_clause(%With{items: items}), do: "WITH #{Enum.join(items, ", ")}"
+  defp render_clause(%Set{expression: e}), do: "SET #{e}"
+  defp render_clause(%Remove{items: items}), do: "REMOVE #{Enum.join(items, ", ")}"
+  defp render_clause(%Delete{items: items}), do: "DELETE #{Enum.join(items, ", ")}"
+  defp render_clause(%DetachDelete{items: items}), do: "DETACH DELETE #{Enum.join(items, ", ")}"
   defp render_clause(%Return{items: items}), do: "RETURN #{Enum.join(items, ", ")}"
   defp render_clause(%Skip{value: n}), do: "SKIP #{n}"
   defp render_clause(%Limit{value: n}), do: "LIMIT #{n}"
@@ -281,6 +290,11 @@ defmodule AshNeo4j.Cypher do
     end
 
     bolty_result
+  end
+
+  def run_expecting_deletions(%Query{} = query) do
+    {cypher, params} = render(query)
+    run_expecting_deletions(cypher, params)
   end
 
   def run_expecting_deletions(cypher, params \\ %{}) when is_bitstring(cypher) do
